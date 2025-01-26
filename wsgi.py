e6209bf738f26381c20f4004fa246d2a5afb04ef
yungcortex <105908604+yungcortex@users.noreply.github.com>
2025-01-26 14:02:45 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the project directory to the Python path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from app import app
 
 if __name__ == "__main__":
