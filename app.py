c942e557dcc42c4e0f2f35bda7c75d7654437324
yungcortex <105908604+yungcortex@users.noreply.github.com>
2025-01-26 14:03:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import os
 app = Flask(__name__)
 app.register_blueprint(character_blueprint)
 
+# Export the app variable
+__all__ = ['app']
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port) 
