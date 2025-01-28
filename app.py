91f33edb0fb18e70c3c85690be561c471357b15b
yungcortex <105908604+yungcortex@users.noreply.github.com>
2025-01-28 12:59:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
-from characters.test import bp as character_blueprint
+from flask import Flask, render_template
+from characters.test import bp as character_bp
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.register_blueprint(character_blueprint)
+app.register_blueprint(character_bp)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 # Export the app variable
 __all__ = ['app']
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(debug=True) 
